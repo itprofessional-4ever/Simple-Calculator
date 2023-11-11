07212fcad619809ff0f4f807b431869a01cbf752
Chloe <itprofessional.4ever7@gmail.com>
2023-11-10 18:43:52 -0700

    basic calculator

--- MyHelloWorld/main.cpp
+++ MyHelloWorld/main.cpp
@@ -1,14 +1,45 @@
-//
-//  main.cpp
-//  MyHelloWorld
-//
-//  Created by Chloe Lee on 2023-11-06.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "*********** CALCULATOR ***********\n";
+
+    std::cout << "Enter either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout << "Enter #1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter #2: ";
+    std::cin >> num2;
+
+    switch(op){
+        case '+':
+            result = num1 + num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '/':
+            result = num1 / num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        default:
+            std::cout << "That wasn't a valid response\n";
+    }
+
+    std::cout << "**********************************";
+        
     return 0;
 }
